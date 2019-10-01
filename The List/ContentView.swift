43d056f7abaf55f6d731c43a9cc1242dbdd1896a
Eric Foster <eric@efo.st>
2019-09-30 22:22:09 -0700

    Initial Commit

---
+++ The List/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  The List
+//
+//  Created by Eric Foster on 9/30/19.
+//  Copyright © 2019 Eric Foster. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
